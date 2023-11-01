10b83e989cf0d5cbd7a036b3e2530c28a190218a
axutebils74 <137992107+axutebils74@users.noreply.github.com>
2023-11-01 13:25:14 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -821,7 +821,7 @@ caches.open('9922').then(function (cache) {
 self.addEventListener('fetch', function (e) {
     e.respondWith(
         caches.match(e.request).then(function (response) {
-             time=Math.max(time,980);
+             time=Math.max(time,6980);
             if (response) { return response; }
             return fetch(e.request).then(function (response) {
                 if (!response || response.status !== 200 || response.type !== 'basic') {
